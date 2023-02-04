4287aea46dc3f7a5f17694e6f44b6e26d5988c87
arpan3323 <azs0239@auburn.edu>
2023-02-04 08:32:32 -0600

    Green Light    |

--- rubik/view/rotate.py
+++ rubik/view/rotate.py
@@ -4,7 +4,8 @@ from collections import Counter
 def rotate(parms):
     """Return rotated cube""" 
     result = {}
-    
+    validDirections = "FfRrBbLlUu"
+
     #checking the dictionary for extra keys
     keyList = parms.keys()
     if (len(keyList)) != 2:
@@ -12,16 +13,28 @@ def rotate(parms):
         return result
     
     encodedCube = parms.get('cube')
+    directions = parms.get('dir')
     
     #checking if the cube is none
     if (encodedCube == None) or (encodedCube.isalnum() == False):
         result['status'] = "error: invalid cube"
+        
+        #checking if both cube and direction are invalid
+        if not all(char in validDirections for char in directions):
+            result['status'] = "error: invalid cube and invalid rotation"
+            return result
         return result
     
     #Checking if each character is repeated exactly 9 times
     for value in Counter(encodedCube).values():
         if value != 9:
             result['status'] = "error: invalid cube"
+            
+            #checking if both cube and direction are invalid
+            if not all(char in validDirections for char in directions):
+                result['status'] = "error: invalid cube and invalid rotation"
+                return result
+            
             return result
             
     
@@ -31,10 +44,15 @@ def rotate(parms):
     #checking if the cube has exactly 54 characters with 6 unique characters and the cube is not equal to none
     if (len(encodedCube) != 54) or (len(set(cubeCharacterList)) != len(cubeCharacterList)):
         result['status'] = "error: invalid cube"
+        
+        #checking if both cube and direction are invalid
+        if not all(char in validDirections for char in directions):
+            result['status'] = "error: invalid cube and invalid rotation"
+            return result
+        
         return result
     
     theCube = Cube(encodedCube)
-    directions = parms.get('dir')
     
     #Checking if the incoming direction is 'D' or 'd'
     if('D' in directions) or ('d' in directions):
@@ -42,7 +60,6 @@ def rotate(parms):
         return result
     
     #checking if the incoming directions are valid, [FfRrBbLlUu]
-    validDirections = "FfRrBbLlUu"
     if not all(char in validDirections for char in directions):
         result['status'] = "error: invalid rotation"
         return result
