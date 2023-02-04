3407a86781f908ad4934df796bad17d411e72274
arpan3323 <azs0239@auburn.edu>
2023-02-04 04:18:10 -0600

    Green Light    |

--- rubik/view/rotate.py
+++ rubik/view/rotate.py
@@ -8,7 +8,15 @@ def rotate(parms):
     theCube = Cube(encodedCube)
     if len(encodedCube) != 54:
         result['status'] = "error: invalid cube"
-        return result 
+        return result
+    
+    #creating a list that stores 5th, 14th, 23rd, 32nd, 41st, and 50th characters of the cube string
+    cubeCharacterList = [encodedCube[4], encodedCube[13], encodedCube[22], encodedCube[31], encodedCube[40], encodedCube[49]]
+    
+    #checking if all elements in the cubeCharacterList are unique
+    if len(set(cubeCharacterList)) != len(cubeCharacterList):
+        result['status'] = "error: invalid cube"
+        return result
     
     directions = parms.get('dir')
     
