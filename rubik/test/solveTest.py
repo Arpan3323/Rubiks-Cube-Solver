2c3b3b7339b6e2094a7eee68abbac6721cc9e27a
arpan3323 <azs0239@auburn.edu>
2023-02-16 00:18:23 -0600

    Red Light      |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -4,13 +4,49 @@ from rubik.view.solve import solve
 
 class SolveTest(TestCase):
         
-# Happy path
-#    Test that the stubbed solve returns the correct result
-    def test100_solve_returnStubbedSolution(self):
+#Analysis of Solve
+#
+#    Solve: receives a dictionary that contains a cube key with values representing the faces of a cube.
+#           performs rotations on an unsolved cube to solve it. Currently only solves for bottom cross
+#    
+#    output:    
+#        If the input cube is valid :
+#                        {solution':'recorded_rotations', 'status': 'ok', 'integrity':''} 
+#                            where value of 'solution’ describes rotations
+#                            'status' is 'ok'
+#                            'integrity' is ''     
+#        If the input cube is invalid or extraneous keys are present:
+#                        {‘status’: ‘error: xxx’}
+#                            where ‘xxx’ is an error message of the developer’s choosing
+#
+#
+#
+#    Analysis: bottomCross.SolveBottomCross
+#        inputs:
+#            theCube: a string representing the 54 characters of the cube
+#        output: 
+#            side-effects: no external effects, internal changes only
+#            nominal:  returns the rotations performed to achieve bottom face cross orientation
+#            abnormal: 
+#
+#    happy path tests:
+#                test 001: checks if solution contains no rotations when the bottom cross exists
+#
+#
+#    sad path tests:
+#                catching the invalid directions, d and D, in rotateTest
+#                catching invalid cube in rotateTest
+#
+#
+#    evil path test:
+#                none
+
+    def test001_solve_checkIfBottomCrossAlreadyExists(self):
         parms = {}
         parms['cube'] = 'bbbbbbbbbrrrrrrrrrooooooooogggggggggyyyyyyyyywwwwwwwww'
         result = solve(parms)
-        self.assertIn('status', result)
+        self.assertEqual('', result['solution'])
         self.assertEqual('ok', result['status'])
-        self.assertIn('integrity', result)
-        self.assertEqual('FRBLUD', result.get('solution'))
+        self.assertEqual('', result['integrity'])
+
+
