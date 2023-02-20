c6dacc33ecab0b6b7ba1b2ef51f17254a3a0fa2c
arpan3323 <azs0239@auburn.edu>
2023-02-20 02:30:32 -0600

    Green Light    |

--- rubik/controller/bottomCross.py
+++ rubik/controller/bottomCross.py
@@ -34,7 +34,7 @@ def solveBottomCross(theCube: Cube) -> str:
         #front face
         while(cubeList[FTM] != cubeList[FMM]):
             rotation += 'U'
-            theCube._rotateU()
+            cubeList = list(theCube._rotateU())
         if (cubeList[FTM] == cubeList[FMM]):
             rotation += 'FF'
         
