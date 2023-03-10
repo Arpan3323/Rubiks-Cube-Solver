1670f0a1a42b8e9acfbba39da2be7c25ab14163a
arpan3323 <azs0239@auburn.edu>
2023-03-09 22:20:24 -0600

    Green Light    |

--- rubik/controller/bottomLayer.py
+++ rubik/controller/bottomLayer.py
@@ -1,5 +1,6 @@
-import rubik.model.constants
+from rubik.model.constants import *
 from rubik.model.cube import Cube
+import rubik.controller.bottomCross as bc
 
 def solveBottomLayer(theCube: Cube) -> str:
     '''
@@ -9,4 +10,18 @@ def solveBottomLayer(theCube: Cube) -> str:
         input:  an instance of the cube class with the down-face cross solved
         output: the rotations required to solve the bottom layer  
     '''  
+    cubeList = list(theCube.get())
+    
+    if _verifyBottomLayerExists(cubeList):
+        return ''
+    
     return 'R'      #TODO:  remove this stubbed value
+
+def _verifyBottomLayerExists(cubeList):
+    bottomLayerExists = (cubeList[range(FBL, FBR+1)] == cubeList[FMM] and cubeList[range(RBL, RBR+1)] == cubeList[RMM] and 
+                         cubeList[range(BBL, BBR+1)] == cubeList[BMM] and cubeList[range(LBL, LBR+1)] == cubeList[LMM] and
+                         cubeList[range(DTL, DTR+1)] == cubeList[DMM] and cubeList[range(DBL, DBR+1)] == cubeList[DMM] and
+                         cubeList[DMR, DML] == cubeList[DMM])
+    return bottomLayerExists 
+        
+    
