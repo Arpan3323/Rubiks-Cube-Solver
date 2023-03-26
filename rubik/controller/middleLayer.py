b42b0581f2f3ea68c3b8a37f02134c57007008f6
arpan3323 <azs0239@auburn.edu>
2023-03-25 20:50:37 -0500

    Green Light    |

--- rubik/controller/middleLayer.py
+++ rubik/controller/middleLayer.py
@@ -9,6 +9,9 @@ def solveMiddleLayer(theCube: Cube) -> str:
     if verifyMiddleLayerExists(cubeList):
         return ''
     
+    if bl.verifyBottomLayerExists(cubeList) == False:
+        cubeList = _alignToBottomLayer(theCube)
+    
     return 'B'      #TODO:  remove this stubbed value
 
 def verifyMiddleLayerExists(cubeList):
@@ -21,3 +24,7 @@ def verifyMiddleLayerExists(cubeList):
            and cubeList[BMM] == cubeList[BMR]
            and cubeList[LMM] == cubeList[LML]
            and cubeList[LMM] == cubeList[LMR])
+
+def _alignToBottomLayer(theCube):
+    bottomLayerCube = bl.solveBottomLayer(theCube)[0]
+    return bottomLayerCube
