931a36429d6cce13b697dff965e763f631af164c
arpan3323 <azs0239@auburn.edu>
2023-03-25 20:23:24 -0500

    Red Light      |

--- rubik/test/middleLayerTest.py
+++ rubik/test/middleLayerTest.py
@@ -13,6 +13,7 @@ from rubik.view.solve import solve
 
 #Happy path tests:
 #    test 100: checks the rotations returned if middle layer is solved
+#    test 101: aligns the incoming cube to bottom layer configuration if it does not exists
 
 
 
@@ -25,3 +26,10 @@ class middleLayerTest(unittest.TestCase):
         actualRotation = ml.solveMiddleLayer(theCube)
         expectedRotations = ''
         self.assertEquals(actualRotation, expectedRotations)
+        
+    def test100_middleLayer_alignToBottomLayerIfItDoesNotExists(self):
+        encodedCube = 'ywgggrwygrrwbowrrbrbobbgwybboorrwyooyybgywgowggybwyroo'
+        expectedCube = 'grbbgggggrryyoyoooggbobobbbybrbrrrrroooyygyyywwwwwwwww'
+        theCube = cube.Cube(encodedCube)
+        actualCube = ''.join(ml._alignToBottomLayer(theCube))
+        self.assertEquals(actualCube, expectedCube)
