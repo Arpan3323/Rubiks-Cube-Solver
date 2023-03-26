800a204be42b9951270e929cb147721c392e3e59
arpan3323 <azs0239@auburn.edu>
2023-03-25 20:10:08 -0500

    Green Light    |

--- rubik/controller/middleLayer.py
+++ rubik/controller/middleLayer.py
@@ -1,12 +1,23 @@
-import rubik.model.constants
+from rubik.model.constants import *
 from rubik.model.cube import Cube
+import rubik.controller.bottomLayer as bl
 
 def solveMiddleLayer(theCube: Cube) -> str:
-    '''
-        This is the top-level function  for rotating
-        a cube so that the middle layer is solved.
-        
-        input:  an instance of the cube class with the bottom layer solved
-        output: the rotations required to solve the middle layer  
-    '''  
+
+    cubeList = list(theCube.get())
+
+    if verifyMiddleLayerExists(cubeList):
+        return ''
+    
     return 'B'      #TODO:  remove this stubbed value
+
+def verifyMiddleLayerExists(cubeList):
+    return(bl.verifyBottomLayerExists(cubeList) 
+           and cubeList[FMM] == cubeList[FML]
+           and cubeList[FMM] == cubeList[FMR]
+           and cubeList[RMM] == cubeList[RML]
+           and cubeList[RMM] == cubeList[RMR]
+           and cubeList[BMM] == cubeList[BML]
+           and cubeList[BMM] == cubeList[BMR]
+           and cubeList[LMM] == cubeList[LML]
+           and cubeList[LMM] == cubeList[LMR])
