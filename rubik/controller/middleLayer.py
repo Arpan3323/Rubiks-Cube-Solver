55d2b38ced3dbab1fa450de1f7879d8fbf7fd458
arpan3323 <azs0239@auburn.edu>
2023-03-26 01:10:42 -0500

    Green Light    |

--- rubik/controller/middleLayer.py
+++ rubik/controller/middleLayer.py
@@ -30,6 +30,7 @@ def _alignToBottomLayer(theCube):
     return theCube.rotate(bottomLayerRotations)
 
 def _checkTopLayerForEdgePair(cubeList):
+    topLayerEdgePairRotations = ''
     sideAndTopEdgePairs = [(FTM, UBM), (RTM, UMR), (BTM, UTM), (LTM, UML)]
     pairFound = False
     for edgePair in sideAndTopEdgePairs:
@@ -37,20 +38,94 @@ def _checkTopLayerForEdgePair(cubeList):
             requiredSideEdge = edgePair[0]
             #requiredTopEdge = edgePair[1]
             pairFound = True
-    if cubeList[requiredSideEdge] != cubeList[FMM]:
-        sideEdgeToCenterRotations, requiredTopEdge = sideEdgeAlignmentRotations(requiredSideEdge, cubeList)
-    cubeList = Cube(cubeList).rotate(sideEdgeToCenterRotations)
-    return cubeList, sideEdgeToCenterRotations
+    sideEdgeToCenterRotations, requiredTopEdge = sideEdgeAlignmentRotations(requiredSideEdge, cubeList)
+    if sideEdgeToCenterRotations != '':
+        topLayerEdgePairRotations += sideEdgeToCenterRotations
+        cubeList = Cube(cubeList).rotate(sideEdgeToCenterRotations)
+    topEdgeRotations = topEdgeAlignmentRotations(requiredTopEdge, cubeList)
+    topLayerEdgePairRotations += topEdgeRotations
+    cubeList = Cube(cubeList).rotate(topEdgeRotations)
+
+
+    return cubeList, topLayerEdgePairRotations
 
 
 def sideEdgeAlignmentRotations(requiredSideEdge, cubeList):
-    if cubeList[requiredSideEdge] == cubeList[RMM]:
-        rotations = 'UUU'
-        requiredTopEdgeLocation = UMR
-    elif cubeList[requiredSideEdge] == cubeList[BMM]:
-        rotations = 'UU'
-        requiredTopEdgeLocation = UTM
-    elif cubeList[requiredSideEdge] == cubeList[LMM]:
-        rotations = 'U'
-        requiredTopEdgeLocation = UML
+    if requiredSideEdge == FTM:
+        if cubeList[requiredSideEdge] == cubeList[FMM]:
+            rotations = ''
+            requiredTopEdgeLocation = UBM
+        if cubeList[requiredSideEdge] == cubeList[RMM]:
+            rotations = 'UUU'
+            requiredTopEdgeLocation = UMR
+        elif cubeList[requiredSideEdge] == cubeList[BMM]:
+            rotations = 'UU'
+            requiredTopEdgeLocation = UTM
+        elif cubeList[requiredSideEdge] == cubeList[LMM]:
+            rotations = 'U'
+            requiredTopEdgeLocation = UML
+    if requiredSideEdge == RTM:
+        if cubeList[requiredSideEdge] == cubeList[RMM]:
+            rotations = ''
+            requiredTopEdgeLocation = UMR
+        elif cubeList[requiredSideEdge] == cubeList[BMM]:
+            rotations = 'UUU'
+            requiredTopEdgeLocation = UTM
+        elif cubeList[requiredSideEdge] == cubeList[LMM]:
+            rotations = 'UU'
+            requiredTopEdgeLocation = UML
+        elif cubeList[requiredSideEdge] == cubeList[FMM]:
+            rotations = 'U'
+            requiredTopEdgeLocation = UBM
+    if requiredSideEdge == BTM:
+        if cubeList[requiredSideEdge] == cubeList[BMM]:
+            rotations = ''
+            requiredTopEdgeLocation = UTM
+        elif cubeList[requiredSideEdge] == cubeList[LMM]:
+            rotations = 'UUU'
+            requiredTopEdgeLocation = UML
+        elif cubeList[requiredSideEdge] == cubeList[FMM]:
+            rotations = 'UU'
+            requiredTopEdgeLocation = UBM
+        elif cubeList[requiredSideEdge] == cubeList[RMM]:
+            rotations = 'U'
+            requiredTopEdgeLocation = UMR
+    if requiredSideEdge == LTM:
+        if cubeList[requiredSideEdge] == cubeList[LMM]:
+            rotations = ''
+            requiredTopEdgeLocation = UML
+        elif cubeList[requiredSideEdge] == cubeList[FMM]:
+            rotations = 'UUU'
+            requiredTopEdgeLocation = UBM
+        elif cubeList[requiredSideEdge] == cubeList[RMM]:
+            rotations = 'UU'
+            requiredTopEdgeLocation = UMR
+        elif cubeList[requiredSideEdge] == cubeList[BMM]:
+            rotations = 'U'
+            requiredTopEdgeLocation = UTM
     return rotations, requiredTopEdgeLocation
+
+def topEdgeAlignmentRotations(requiredTopEdge, cubeList):
+    if requiredTopEdge == UBM:
+        if cubeList[requiredTopEdge] == cubeList[LMM]:
+            rotation = 'u'
+        elif cubeList[requiredTopEdge] == cubeList[RMM]:
+            rotation = 'U'
+    if requiredTopEdge == UMR:
+        if cubeList[requiredTopEdge] == cubeList[FMM]:
+            rotation = 'u'
+        elif cubeList[requiredTopEdge] == cubeList[BMM]:
+            rotation = 'U'
+    if requiredTopEdge == UTM:
+        if cubeList[requiredTopEdge] == cubeList[RMM]:
+            rotation = 'u'
+        elif cubeList[requiredTopEdge] == cubeList[LMM]:
+            rotation = 'U'
+    if requiredTopEdge == UML:
+        if cubeList[requiredTopEdge] == cubeList[BMM]:
+            rotation = 'u'
+        elif cubeList[requiredTopEdge] == cubeList[FMM]:
+            rotation = 'U'
+    return rotation 
+
+
