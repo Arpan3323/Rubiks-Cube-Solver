162302c1ed575767286a974ed782d63567c76f61
arpan3323 <azs0239@auburn.edu>
2023-04-10 02:03:25 -0500

    Green Light    |

--- rubik/controller/upFaceCross.py
+++ rubik/controller/upFaceCross.py
@@ -1,12 +1,18 @@
-import rubik.model.constants
+from rubik.model.constants import *
 from rubik.model.cube import Cube
+import rubik.controller.middleLayer as ml
 
 def solveUpCross(theCube: Cube) -> str:
-    '''
-        This is the top-level function  for rotating
-        a cube into the up-face cross configuration.
-        
-        input:  an instance of the cube class with the middle layer solved
-        output: the rotations required to solve the up-face cross  
-    '''  
+    cubeString = theCube.get()
+
+    if verifyTopCrossExists(cubeString):
+        return [cubeString, '']
+      
     return 'L'      #TODO:  remove this stubbed value
+
+def verifyTopCrossExists(cubeString):
+    return(ml.verifyMiddleLayerExists(cubeString)
+           and cubeString[UBM] == cubeString[UMM]
+           and cubeString[UMR] == cubeString[UMM]
+           and cubeString[UTM] == cubeString[UMM]
+           and cubeString[UML] == cubeString[UMM])
