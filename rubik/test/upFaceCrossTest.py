3354976317d791d69a9a9c8af2960bbc28364487
arpan3323 <azs0239@auburn.edu>
2023-04-10 02:16:24 -0500

    Red Light      |

--- rubik/test/upFaceCrossTest.py
+++ rubik/test/upFaceCrossTest.py
@@ -15,14 +15,23 @@ from rubik.view.solve import solve
 #Happy Path Tests
 # test 100: check rotations returned when up face already has a cross
 
+# test 101: align the incoming cube to middleLayer configuration if it does not already exist
+
 
 
 class upFaceCrossTest(unittest.TestCase):
 
 
-    def test100_middleLayer_checkIfMiddleLayerIsSolved(self):
+    def test100_solveUpCross_checkIfUpFaceCrossIsSolved(self):
         encodedCube = 'rgyggggggoogooooooorybbbbbbbbyrrrrrrryyyyygybwwwwwwwww'
         theCube = cube.Cube(encodedCube)
         actualRotation = ufc.solveUpCross(theCube)[1]
         expectedRotations = ''
         self.assertEquals(actualRotation, expectedRotations)
+
+    def test101_solveUpCross_alignToMiddleLayer(self):
+        encodedCube = 'wyyoggbgrrrbroogyoobgbbywryogbbrwbgrywwyywrogyowwwrobg'
+        expectedCube = 'rbrggggggyogoooooooyobbbbbbbyyrrrrrryrygyygybwwwwwwwww'
+        theCube = cube.Cube(encodedCube)
+        actualCube = ufc._alignMiddleLayer(theCube)
+        self.assertEquals(expectedCube, actualCube)
