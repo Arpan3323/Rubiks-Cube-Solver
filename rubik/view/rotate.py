1c8bd9dcb439010375ef5bb26fbc8b2a5f98f739
arpan3323 <azs0239@auburn.edu>
2023-04-12 03:44:35 -0500

    Refactor       |

--- rubik/view/rotate.py
+++ rubik/view/rotate.py
@@ -5,24 +5,16 @@ from rubik.model.constants import *
 def rotate(parms):
     """Return rotated cube""" 
     result = {}
-    
-    #creating variables for mandatory cube characters and valid directions
     validDirections = "FfRrBbLlUu"
-    mandatoryCharsInCube = 54
-    numberOfKeys = 2
-
-    #checking the dictionary for extra keys
-    keyList = parms.keys()
-    if (len(keyList)) > numberOfKeys:
+    
+    if _isExtraKey(parms):
         result['status'] = "error: extraneous key detected"
         return result
     
     encodedCube = parms.get('cube')
     directions = parms.get('dir')
     theCube = Cube(encodedCube)
-    
-    #checking if the cube cube is empty, None, consists of characters outside [A-Z][a-z][0-9], or 
-    #contains exactly 54 characters
+
     if ((theCube.validateCube(encodedCube) == False) 
         or (theCube.validateUniqueCenters(encodedCube) == False)
         or (theCube.validateNineOfEachCubeCharacters(encodedCube) == False)):
@@ -30,7 +22,6 @@ def rotate(parms):
             if (directions != None) and (not all(char in validDirections for char in directions)):
                 result['status'] = "error: invalid cube and invalid rotation"
             return result   
-     
     elif (directions != None) and (not all(char in validDirections for char in directions)):
         result['status'] = "error: invalid rotation"
         return result
@@ -40,3 +31,8 @@ def rotate(parms):
     result['cube'] = theCube.get()
     result['status'] = 'ok'                     
     return result
+
+def _isExtraKey(parms):
+    numberOfAllowedKeys = 2
+    keyList = parms.keys()
+    return (len(keyList)) > numberOfAllowedKeys
