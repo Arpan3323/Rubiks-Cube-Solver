86a2fe40c146440a8e83dc629d2da5f74f074e69
arpan3323 <azs0239@auburn.edu>
2023-04-15 06:12:30 -0500

    Green Light    |

--- rubik/view/solve.py
+++ rubik/view/solve.py
@@ -29,8 +29,8 @@ def solve(parms):
     rotations += solveBottomCross(theCube)      #iteration 2
     rotations += solveBottomLayer(theCube)[1]   #iteration 3
     rotations += solveMiddleLayer(theCube)[1]     #iteration 4
-    rotations += solveUpCross(_cubeAfterEachIncrement(encodedCube, rotations))[1]          #iteration 5
-    rotations += solveUpSurface(_cubeAfterEachIncrement(encodedCube, rotations))[1]        #iteration 5
+    rotations += solveUpCross(theCube)[1]          #iteration 5
+    rotations += solveUpSurface(theCube)[1]        #iteration 5
     #rotations += solveUpperLayer(theCube)       #iteration 6
     
     result['solution'] = rotations
